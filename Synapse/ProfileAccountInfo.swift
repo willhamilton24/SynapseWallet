0979585d74a2ccddf3e5e972e8b5c4153cfa0851
Will Hamilton <ishi121@icloud.com>
2019-10-27 12:39:39 -0400

    Profile Page Close Button and Reformating

---
+++ Synapse/ProfileAccountInfo.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileAccountInfo.swift
+//  Synapse
+//
+//  Created by Will Hamilton on 10/26/19.
+//  Copyright © 2019 Comitatus Capital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileAccountInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileAccountInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileAccountInfo()
+    }
+}
---
+++ Synapse/ProfilePicture.swift
@@ -0,0 +1,21 @@
+//
+//  ProfilePicture.swift
+//  Synapse
+//
+//  Created by Will Hamilton on 10/26/19.
+//  Copyright © 2019 Comitatus Capital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfilePicture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilePicture_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePicture()
+    }
+}
